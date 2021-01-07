88898117e0a58ac108b3ad858da289232a709625
scarycoffee <8727952+scarycoffee@users.noreply.github.com>
2021-01-07 05:20:13 +0000

    keymap, move camera code to File

--- main.go
+++ main.go
@@ -9,12 +9,10 @@ import (
 )
 
 var (
-	camera rl.Camera2D
-	// canvas rl.RenderTexture2D
 	target rl.Vector2
 )
 
-type Keymap map[string]rl.Key
+type Keymap map[string][]rl.Key
 
 // Tool implementations should call rl.DrawPixel or other operations, there are
 // no canvas middleware
@@ -30,14 +28,19 @@ type Tool interface {
 	DrawPreview(x, y int)
 }
 
+// Layer has a Canvas and initialFill keeps track of if it's been filled on
+// creation
 type Layer struct {
 	Canvas      rl.RenderTexture2D
 	initialFill bool
 }
 
+// File handles all of the actions for a file. Multiple files can be created
+// and handled at the same time
 type File struct {
-	// Layers belonging to the canvas. The last one is for tool previews
-	Layers       []*Layer
+	Camera rl.Camera2D
+
+	Layers       []*Layer // The last one is for tool previews
 	CurrentLayer int
 
 	CurrentTool    Tool
@@ -48,111 +51,140 @@ type File struct {
 	keyMovable     bool
 	lastKey        []rl.Key
 
-	Zoom float32 // Camera zoom for pixel movement
+	// Probably a cleaner way to handle mouse relational movement...
+	mouseX, mouseY, mouseLastX, mouseLastY int
 
 	Keymap Keymap
 }
 
 // Update checks for input and uses the current tool to draw to the current
 // layer
-func (c *File) Update() {
-	layer := c.Layers[c.CurrentLayer]
+func (f *File) Update() {
+	layer := f.Layers[f.CurrentLayer]
 
+	// Update camera
+	// TODO zoom at cursor location, not target/offset
+	f.Camera.Zoom += float32(rl.GetMouseWheelMove()) * 0.1 * f.Camera.Zoom
+
+	f.Camera.Offset.X = float32(rl.GetScreenWidth()) / 2
+	f.Camera.Offset.Y = float32(rl.GetScreenHeight()) / 2
+	// Move target
+	f.mouseX = rl.GetMouseX()
+	f.mouseY = rl.GetMouseY()
+	if rl.IsMouseButtonDown(rl.MouseMiddleButton) {
+		target.X += float32(f.mouseLastX-f.mouseX) / f.Camera.Zoom
+		target.Y += float32(f.mouseLastY-f.mouseY) / f.Camera.Zoom
+	}
+	f.mouseLastX = f.mouseX
+	f.mouseLastY = f.mouseY
+	f.Camera.Target = target
+
+	// Draw
 	rl.BeginTextureMode(layer.Canvas)
 	if !layer.initialFill {
 		rl.ClearBackground(rl.DarkGray)
 		layer.initialFill = true
 	}
 
-	c.keyRepeatTimer += rl.GetFrameTime() * 1000
-	if c.keyRepeatTimer > float32(c.KeyRepeat.Milliseconds()) {
-		c.keyRepeatTimer = 0
-		c.keyMovable = true
+	f.keyRepeatTimer += rl.GetFrameTime() * 1000
+	if f.keyRepeatTimer > float32(f.KeyRepeat.Milliseconds()) {
+		f.keyRepeatTimer = 0
+		f.keyMovable = true
 	}
 
 	// Stack keys up so that if left is held, then right is held, then right
 	// is released, the cursor would continue going left instead of staying
 	// still
-	if rl.IsKeyPressed(rl.KeyN) {
-		c.keyMovable = true
-		c.lastKey = append(c.lastKey, rl.KeyN)
+	checkDown := func(keys []rl.Key) {
+		for _, key := range keys {
+			if rl.IsKeyPressed(key) {
+				f.keyMovable = true
+				f.lastKey = append(f.lastKey, key)
+			}
+		}
 	}
-	if rl.IsKeyPressed(rl.KeyH) {
-		c.keyMovable = true
-		c.lastKey = append(c.lastKey, rl.KeyH)
-	}
-	if rl.IsKeyPressed(rl.KeyC) {
-		c.keyMovable = true
-		c.lastKey = append(c.lastKey, rl.KeyC)
-	}
-	if rl.IsKeyPressed(rl.KeyT) {
-		c.keyMovable = true
-		c.lastKey = append(c.lastKey, rl.KeyT)
-	}
+	checkDown(f.Keymap["toolRight"])
+	checkDown(f.Keymap["toolLeft"])
+	checkDown(f.Keymap["toolDown"])
+	checkDown(f.Keymap["toolUp"])
 
-	if len(c.lastKey) > 0 && rl.IsKeyDown(c.lastKey[len(c.lastKey)-1]) {
-		last := c.lastKey[len(c.lastKey)-1]
-		if c.keyMovable {
-			c.keyRepeatTimer = 0
-			c.keyMovable = false
+	if len(f.lastKey) > 0 && rl.IsKeyDown(f.lastKey[len(f.lastKey)-1]) {
+		last := f.lastKey[len(f.lastKey)-1]
+		if f.keyMovable {
+			f.keyRepeatTimer = 0
+			f.keyMovable = false
 
-			moveAmount := int(c.Zoom)
+			moveAmount := int(f.Camera.Zoom)
 			x := rl.GetMouseX()
 			y := rl.GetMouseY()
 
+			matches := func(match rl.Key, keys []rl.Key) bool {
+				for _, key := range keys {
+					if key == match {
+						return true
+					}
+				}
+				return false
+			}
 			// TODO move amount based on zoom
-			switch last {
-			case c.Keymap["toolRight"]:
+			switch {
+			case matches(last, f.Keymap["toolRight"]):
 				rl.SetMousePosition(x+moveAmount, y)
-			case c.Keymap["toolLeft"]:
+			case matches(last, f.Keymap["toolLeft"]):
 				rl.SetMousePosition(x-moveAmount, y)
-			case c.Keymap["toolDown"]:
+			case matches(last, f.Keymap["toolDown"]):
 				rl.SetMousePosition(x, y+moveAmount)
-			case c.Keymap["toolUp"]:
+			case matches(last, f.Keymap["toolUp"]):
 				rl.SetMousePosition(x, y-moveAmount)
 			}
 		}
 	} else {
 		// Pop lastKey until we find a key that's still down
-		if len(c.lastKey) > 0 {
-			c.lastKey = c.lastKey[:len(c.lastKey)-1]
+		if len(f.lastKey) > 0 {
+			f.lastKey = f.lastKey[:len(f.lastKey)-1]
 		}
-		c.keyRepeatTimer = 0
-		c.keyMovable = true
+		f.keyRepeatTimer = 0
+		f.keyMovable = true
 	}
 
-	cursor := rl.GetScreenToWorld2D(rl.GetMousePosition(), camera)
+	cursor := rl.GetScreenToWorld2D(rl.GetMousePosition(), f.Camera)
 	cursor = cursor.Add(rl.NewVector2(float32(layer.Canvas.Texture.Width)/2, float32(layer.Canvas.Texture.Height)/2))
 	if rl.IsMouseButtonDown(rl.MouseLeftButton) {
-		c.HasDoneMouseUp = false
-		c.CurrentTool.MouseDown(int(cursor.X), int(cursor.Y))
+		f.HasDoneMouseUp = false
+		f.CurrentTool.MouseDown(int(cursor.X), int(cursor.Y))
 	} else {
-		if c.HasDoneMouseUp == false {
-			c.HasDoneMouseUp = true
-			c.CurrentTool.MouseUp(int(cursor.X), int(cursor.Y))
+		if f.HasDoneMouseUp == false {
+			f.HasDoneMouseUp = true
+			f.CurrentTool.MouseUp(int(cursor.X), int(cursor.Y))
 		}
 	}
 	rl.EndTextureMode()
 
-	rl.BeginTextureMode(c.Layers[len(c.Layers)-1].Canvas)
-	c.CurrentTool.DrawPreview(int(cursor.X), int(cursor.Y))
+	rl.BeginTextureMode(f.Layers[len(f.Layers)-1].Canvas)
+	f.CurrentTool.DrawPreview(int(cursor.X), int(cursor.Y))
 	rl.EndTextureMode()
 }
 
 // Draw is used to draw all of the layers
-func (c *File) Draw() {
-	for _, layer := range c.Layers {
+func (f *File) Draw() {
+	rl.BeginMode2D(f.Camera)
+	for _, layer := range f.Layers {
 		rl.DrawTextureRec(layer.Canvas.Texture,
 			rl.NewRectangle(0, 0, float32(layer.Canvas.Texture.Width), -float32(layer.Canvas.Texture.Height)),
 			rl.NewVector2(-float32(layer.Canvas.Texture.Width)/2, -float32(layer.Canvas.Texture.Height)/2),
 			rl.White)
 	}
+	rl.EndMode2D()
 }
-func (c *File) Destroy() {
-	for _, layer := range c.Layers {
+
+// Destroy unloads each layer's canvas
+func (f *File) Destroy() {
+	for _, layer := range f.Layers {
 		layer.Canvas.Unload()
 	}
 }
+
+// NewFile is the constructor for File
 func NewFile(keymap Keymap) *File {
 	return &File{
 		Layers: []*Layer{
@@ -162,8 +194,8 @@ func NewFile(keymap Keymap) *File {
 		CurrentTool:    &tools.PixelBrushTool{Color: rl.Red},
 		HasDoneMouseUp: true,
 		KeyRepeat:      time.Second / 5,
-		Zoom:           1,
 		Keymap:         keymap,
+		Camera:         rl.Camera2D{Zoom: 8.0},
 	}
 }
 
@@ -176,36 +208,15 @@ func main() {
 	rl.SetTargetFPS(120)
 
 	keymap := Keymap{
-		"toolLeft":  rl.KeyH,
-		"toolRight": rl.KeyN,
-		"toolUp":    rl.KeyC,
-		"toolDown":  rl.KeyT,
+		"toolLeft":  {rl.KeyH, rl.KeyLeft},
+		"toolRight": {rl.KeyN, rl.KeyRight},
+		"toolUp":    {rl.KeyC, rl.KeyUp},
+		"toolDown":  {rl.KeyT, rl.KeyDown},
 	}
 
 	file := NewFile(keymap)
 
-	camera = rl.Camera2D{}
-	camera.Zoom = 8.0
-
-	var mouseX, mouseY, mouseLastX, mouseLastY int
 	for !rl.WindowShouldClose() {
-
-		// TODO zoom at cursor location, not target/offset
-		camera.Zoom += float32(rl.GetMouseWheelMove()) * 0.1 * camera.Zoom
-		file.Zoom = camera.Zoom
-
-		camera.Offset.X = float32(rl.GetScreenWidth()) / 2
-		camera.Offset.Y = float32(rl.GetScreenHeight()) / 2
-		// Move target
-		mouseX = rl.GetMouseX()
-		mouseY = rl.GetMouseY()
-		if rl.IsMouseButtonDown(rl.MouseMiddleButton) {
-			target.X += float32(mouseLastX-mouseX) / camera.Zoom
-			target.Y += float32(mouseLastY-mouseY) / camera.Zoom
-		}
-		mouseLastX = mouseX
-		mouseLastY = mouseY
-		camera.Target = target
 
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
@@ -214,10 +225,8 @@ func main() {
 		file.Update()
 
 		// Draw the file.Canvas, use the camera to draw file.Canvas in the correct place
-		rl.BeginMode2D(camera)
 		file.Draw()
 
-		rl.EndMode2D()
 		rl.EndDrawing()
 	}
 
