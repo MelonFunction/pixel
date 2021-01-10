38affd2fd51a898d0ef8c9787f51cfd13fcfd5dd
scarycoffee <8727952+scarycoffee@users.noreply.github.com>
2021-01-10 04:47:32 +0000

    added support for more keyboard actions

--- main.go
+++ main.go
@@ -5,24 +5,39 @@ import (
 	"time"
 
 	rl "github.com/lachee/raylib-goplus/raylib"
-	"github.com/scarycoffee/pixel/tools"
 )
 
 var (
-	target rl.Vector2
+	target                rl.Vector2
+	keysExemptFromRelease = []rl.Key{
+		rl.KeyLeftControl,
+	}
 )
 
-type Keymap map[string][]rl.Key
+type Pixel struct {
+	Color rl.Color
+}
+
+type Keymap map[string][][]rl.Key
+
+type Action struct {
+	// Tool is the tool used for the action (GetName is used in history panel)
+	Tool Tool
+
+	LastPixelState map[IntVec2]Pixel
+}
 
 // Tool implementations should call rl.DrawPixel or other operations, there are
 // no canvas middleware
-// All tools need MouseDown and MouseUp, but SetColor and GetColor are for
-// the majority of functions as they will need it
 type Tool interface {
+	// Used by every tool
 	MouseDown(x, y int) // Called each frame the mouse is down
 	MouseUp(x, y int)   // Called once, when the mouse button is released
+
+	// Used by drawing tools
 	SetColor(rl.Color)
 	GetColor() rl.Color
+
 	// Takes the current mouse position. Called every frame the tool is
 	// selected. Draw calls are drawn on the preview layer.
 	DrawPreview(x, y int)
@@ -33,6 +48,17 @@ type Tool interface {
 type Layer struct {
 	Canvas      rl.RenderTexture2D
 	initialFill bool
+
+	// PixelData is the "raw" pixels map
+	PixelData map[IntVec2]Pixel
+}
+
+func NewLayer(width, height int, shouldFill bool) *Layer {
+	return &Layer{
+		Canvas:      rl.LoadRenderTexture(width, height),
+		initialFill: shouldFill,
+		PixelData:   make(map[IntVec2]Pixel),
+	}
 }
 
 // File handles all of the actions for a file. Multiple files can be created
@@ -52,6 +78,10 @@ type File struct {
 	keyRepeatTimer float32
 	keyMovable     bool
 	lastKey        []rl.Key
+	// current keys down, used for combinations
+	keysDown map[rl.Key]bool
+	// keys which need to be released before they can be used again
+	keysAwaitingRelease map[rl.Key]bool
 
 	// Probably a cleaner way to handle mouse relational movement...
 	mouseX, mouseY, mouseLastX, mouseLastY int
@@ -90,27 +120,70 @@ func (f *File) Update() {
 		layer.initialFill = true
 	}
 
+	// Handle keyboard actions
+	for key := range f.keysAwaitingRelease {
+		if !rl.IsKeyDown(key) {
+			delete(f.keysAwaitingRelease, key)
+		}
+	}
+
+	checkDown := func(keySlices [][]rl.Key) bool {
+		for _, keySlice := range keySlices {
+			for _, key := range keySlice {
+				isDown := rl.IsKeyDown(key)
+				f.keysDown[key] = isDown
+				needsRelease, ok := f.keysAwaitingRelease[key]
+				if !isDown || (ok && needsRelease) {
+					return false
+				}
+			}
+		}
+		return true
+	}
+	setAwaitingRelease := func(keySlices [][]rl.Key) bool {
+		for _, keySlice := range keySlices {
+			for _, key := range keySlice {
+				found := false
+				for _, k := range keysExemptFromRelease {
+					if k == key {
+						found = true
+					}
+				}
+				if !found {
+					f.keysAwaitingRelease[key] = true
+				}
+			}
+		}
+		return true
+	}
+	// If checkDown is true, then execute setAwaitingRelease (return isn't important)
+	switch {
+	case checkDown(f.Keymap["undo"]) && setAwaitingRelease(f.Keymap["undo"]):
+		f.Undo()
+	}
+
 	f.keyRepeatTimer += rl.GetFrameTime() * 1000
 	if f.keyRepeatTimer > float32(f.KeyRepeat.Milliseconds()) {
 		f.keyRepeatTimer = 0
 		f.keyMovable = true
 	}
-
 	// Stack keys up so that if left is held, then right is held, then right
 	// is released, the cursor would continue going left instead of staying
 	// still
-	checkDown := func(keys []rl.Key) {
-		for _, key := range keys {
-			if rl.IsKeyPressed(key) {
-				f.keyMovable = true
-				f.lastKey = append(f.lastKey, key)
+	checkDownAddStack := func(keySlices [][]rl.Key) {
+		for _, keySlice := range keySlices {
+			for _, key := range keySlice {
+				if rl.IsKeyPressed(key) {
+					f.keyMovable = true
+					f.lastKey = append(f.lastKey, key)
+				}
 			}
 		}
 	}
-	checkDown(f.Keymap["toolRight"])
-	checkDown(f.Keymap["toolLeft"])
-	checkDown(f.Keymap["toolDown"])
-	checkDown(f.Keymap["toolUp"])
+	checkDownAddStack(f.Keymap["toolRight"])
+	checkDownAddStack(f.Keymap["toolLeft"])
+	checkDownAddStack(f.Keymap["toolDown"])
+	checkDownAddStack(f.Keymap["toolUp"])
 
 	if len(f.lastKey) > 0 && rl.IsKeyDown(f.lastKey[len(f.lastKey)-1]) {
 		last := f.lastKey[len(f.lastKey)-1]
@@ -122,10 +195,12 @@ func (f *File) Update() {
 			x := rl.GetMouseX()
 			y := rl.GetMouseY()
 
-			matches := func(match rl.Key, keys []rl.Key) bool {
-				for _, key := range keys {
-					if key == match {
-						return true
+			matches := func(match rl.Key, keySlices [][]rl.Key) bool {
+				for _, keySlice := range keySlices {
+					for _, key := range keySlice {
+						if key == match {
+							return true
+						}
 					}
 				}
 				return false
@@ -209,6 +284,11 @@ func (f *File) Draw() {
 	rl.EndMode2D()
 }
 
+// Undo an action
+func (f *File) Undo() {
+	log.Println("undoing")
+}
+
 // Destroy unloads each layer's canvas
 func (f *File) Destroy() {
 	for _, layer := range f.Layers {
@@ -218,13 +298,11 @@ func (f *File) Destroy() {
 
 // NewFile is the constructor for File
 func NewFile(keymap Keymap, canvasWidth, canvasHeight, tileWidth, tileHeight int) *File {
-	return &File{
+	f := &File{
 		Layers: []*Layer{
-			{rl.LoadRenderTexture(canvasWidth, canvasHeight), false},
-			{rl.LoadRenderTexture(canvasWidth, canvasHeight), true},
+			NewLayer(canvasWidth, canvasHeight, false),
+			NewLayer(canvasWidth, canvasHeight, true),
 		},
-		LeftTool:            &tools.PixelBrushTool{Color: rl.Red},
-		RightTool:           &tools.PixelBrushTool{Color: rl.Green},
 		HasDoneMouseUpLeft:  true,
 		HasDoneMouseUpRight: true,
 		KeyRepeat:           time.Second / 5,
@@ -234,7 +312,12 @@ func NewFile(keymap Keymap, canvasWidth, canvasHeight, tileWidth, tileHeight int
 		CanvasHeight:        canvasHeight,
 		TileWidth:           tileWidth,
 		TileHeight:          tileHeight,
+		keysDown:            make(map[rl.Key]bool),
+		keysAwaitingRelease: make(map[rl.Key]bool),
 	}
+	f.LeftTool = NewPixelBrushTool(rl.Red, f)
+	f.RightTool = NewPixelBrushTool(rl.Green, f)
+	return f
 }
 
 func main() {
@@ -246,10 +329,11 @@ func main() {
 	rl.SetTargetFPS(120)
 
 	keymap := Keymap{
-		"toolLeft":  {rl.KeyH, rl.KeyLeft},
-		"toolRight": {rl.KeyN, rl.KeyRight},
-		"toolUp":    {rl.KeyC, rl.KeyUp},
-		"toolDown":  {rl.KeyT, rl.KeyDown},
+		"toolLeft":  {{rl.KeyH}, {rl.KeyLeft}},
+		"toolRight": {{rl.KeyN}, {rl.KeyRight}},
+		"toolUp":    {{rl.KeyC}, {rl.KeyUp}},
+		"toolDown":  {{rl.KeyT}, {rl.KeyDown}},
+		"undo":      {{rl.KeyLeftControl, rl.KeyZ}},
 	}
 
 	file := NewFile(keymap, 64, 64, 8, 8)
