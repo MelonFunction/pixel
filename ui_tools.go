22a23918dd1f97a63154dc6b0449c0c0538dbf34
scarycoffee <8727952+scarycoffee@users.noreply.github.com>
2021-04-18 03:23:52 +0100

    make both left and right cursor use the same tool

--- ui_tools.go
+++ ui_tools.go
@@ -15,30 +15,18 @@ func ToolsUICloseEditor() {
 func ToolsUIAddButton() {
 	pencil := NewButtonTexture(rl.NewRectangle(0, 0, UIButtonHeight, UIButtonHeight),
 		"./res/icons/pencil.png", true, func(entity *Entity, button rl.MouseButton) {
-			switch button {
-			case rl.MouseLeftButton:
-				CurrentFile.LeftTool = NewPixelBrushTool("Pixel Brush", false)
-			case rl.MouseRightButton:
-				CurrentFile.RightTool = NewPixelBrushTool("Pixel Brush", false)
-			}
+			CurrentFile.LeftTool = NewPixelBrushTool("Pixel Brush", false)
+			CurrentFile.RightTool = NewPixelBrushTool("Pixel Brush", false)
 		}, nil)
 	eraser := NewButtonTexture(rl.NewRectangle(0, 0, UIButtonHeight, UIButtonHeight),
 		"./res/icons/eraser.png", true, func(entity *Entity, button rl.MouseButton) {
-			switch button {
-			case rl.MouseLeftButton:
-				CurrentFile.LeftTool = NewPixelBrushTool("Eraser", true)
-			case rl.MouseRightButton:
-				CurrentFile.RightTool = NewPixelBrushTool("Eraser", true)
-			}
+			CurrentFile.LeftTool = NewPixelBrushTool("Eraser", true)
+			CurrentFile.RightTool = NewPixelBrushTool("Eraser", true)
 		}, nil)
 	fill := NewButtonTexture(rl.NewRectangle(0, 0, UIButtonHeight, UIButtonHeight),
 		"./res/icons/fill.png", true, func(entity *Entity, button rl.MouseButton) {
-			switch button {
-			case rl.MouseLeftButton:
-				CurrentFile.LeftTool = NewFillTool("Fill")
-			case rl.MouseRightButton:
-				CurrentFile.RightTool = NewFillTool("Fill")
-			}
+			CurrentFile.LeftTool = NewFillTool("Fill")
+			CurrentFile.RightTool = NewFillTool("Fill")
 		}, nil)
 
 	toolsButtons.PushChild(pencil)
